d623f11034e09f09f26489039ff090ecae7ab1b4
Keshav Bahoray <110491299+Keray18@users.noreply.github.com>
2022-11-13 21:25:26 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("NavBar").style.top = "0";
+//   } else {
+//     document.getElementById("NavBar").style.top = "-50px";
+//   }
+// }
